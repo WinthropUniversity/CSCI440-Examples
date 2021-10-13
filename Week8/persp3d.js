2a4de97374a9c360113823df604fb2652fcad66c
rpwiegand <rpwiegand@gmail.com>
2021-10-13 15:12:48 -0400

    Significant changes to separate model, camera, and perspective transformations

--- Week8/persp3d.js
+++ Week8/persp3d.js
@@ -7,56 +7,158 @@
 
 // Ugly global variables
 var gl;
-
 var pointLength;
 
-var transMatrixLoc;
-var transMatrix = mat4( 1, 0, 0, 0,
-                        0, 1, 0, 0,
-                        0, 0, 1, 0,
-                        0, 0, 0, 1 );
-
-// The viewing volume ... changing this changes the perspective
-var viewScale=.1;
-var nr   = -viewScale;
-var fr   = +viewScale;
-var tp   = +viewScale;
-var bm   = -viewScale;
-var lft  = -viewScale;
-var rgt  = +viewScale;
-
+// For the uniform locations in the GPU
+var modelMatrixLoc;
 var perspMatrixLoc;
-var perspMatrix = mat4( 
-  (2*nr)/(rgt-lft),  0,               (rgt+lft)/(rgt-lft),  0,
-  0,                 (2*nr)/(tp-bm),  (tp+bm)/(tp-bm),      0,
-  0,                 0,              -(fr+nr)/(fr-nr),      (-2*fr*nr)/(fr-nr),
-  0,                 0,              -1,                    0);
+var cameraMatrixLoc;
 
-// Convenion cos/sin
-var angle = 60;
-var cs = Math.cos(angle*Math.PI/180.0);
-var sn = Math.sin(angle*Math.PI/180.0);
+// Just for convenience while testing
+var identity = mat4( 1, 0 ,0 ,0,
+                     0, 1, 0, 0,
+                     0, 0, 1, 0,
+                     0, 0 ,0 , 1);
 
-// Some rotation matrices
+/**
+ * Basic rotation transformation of the model.  Rotate around x, then y, then z.
+ * @param {*} zRotationAngle Angle (in degrees) to rotate model around x-axis
+ * @param {*} yRotationAngle Angle (in degrees) to rotate model around y-axis
+ * @param {*} zRotationAngle Angle (in degrees) to rotate model around z-axis
+ * @returns 
+ */
+function GetModelTransformationMatrix(xRotationAngle, yRotationAngle, zRotationAngle) {
+  var alpha = xRotationAngle *Math.PI/180.0;  // Rotation around x in radians
+  var phi   = yRotationAngle *Math.PI/180.0;  // Rotation around y in radians
+  var theta = zRotationAngle *Math.PI/180.0;  // Rotation around z in radians
 
-var rz = mat4( cs,  -sn,   0.0,  0.0,
-               sn,   cs,   0.0,  0.0,
-               0.0,  0.0,  1.0,  0.0,
-               0.0,  0.0,  0.0,  1.0 );
-var rx = mat4( 1.0,  0.0,  0.0,  0.0,
-               0.0,  cs,  -sn,   0.0,
-               0.0,  sn,   cs,   0.0,
-               0.0,  0.0,  0.0,  1.0 );
-var ry = mat4( cs,   0.0,  sn,   0.0,
-               0.0,  1.0,  0.0,  0.0,
-              -sn,   0.0,  cs,   0.0,
-               0.0,  0.0,  0.0,  1.0 );
+  var csx = Math.cos(alpha);
+  var snx = Math.sin(alpha);
+  var csy = Math.cos(phi);
+  var sny = Math.sin(phi);
+  var csz = Math.cos(theta);
+  var snz = Math.sin(theta);
 
-// Set the initial transMatrix to be a rotation over Z and over Y so we can see 
-// the pyramid 3D shape ... otherwise, when we look straight down the Z
-// axis, it will look like a square.
-transMatrix = mult(transMatrix,rz);
-transMatrix = mult(transMatrix,ry);
+  // Some standard rotation matrices
+  var rx = mat4( 1.0,  0.0,  0.0,  0.0,
+                 0.0,  csx,  -snx,   0.0,
+                 0.0,  snx,   csx,   0.0,
+                 0.0,  0.0,  0.0,  1.0 );                 
+  var ry = mat4( csy,   0.0,  sny,   0.0,
+                 0.0,  1.0,  0.0,  0.0,
+                -sny,   0.0,  csy,   0.0,
+                 0.0,  0.0,  0.0,  1.0 );
+  var rz = mat4( csz,  -snz,   0.0,  0.0,
+                 snz,   csz,   0.0,  0.0,
+                 0.0,  0.0,  1.0,  0.0,
+                 0.0,  0.0,  0.0,  1.0 );
+ 
+  return ( mult(rz,mult(ry,rx)) );
+}
+
+
+/**
+ * Construct the perspective projection matrix using the field-of-view
+ * method.  Here I compute the aspect ratio directly from the canvas.
+ * 
+ * @param {*} fovy Field of view in the y direction of the camera frame
+ * @param {*} near The near plane of the frustrum
+ * @param {*} far  The far plane of the frustrum
+ */
+function GetPerspectiveProjectionMatrix(fovy, near, far) {
+  var canvas = document.getElementById( "gl-canvas" );
+  var aspectRatio = canvas.width / canvas.height;
+  var fovyRadian = fovy * Math.PI / 180.0;
+  var nr = near;
+  var fr = far;
+  var tp = nr * Math.tan(fovyRadian);
+  var rgt = tp * aspectRatio;
+  //var lft = -rgt;
+  var bt = -bt;
+
+  return ( mat4( 
+    nr/rgt,  0,             0,                     0,
+    0,      nr/tp,          0,                     0,
+    0,      0,              -(fr+nr)/(fr-nr),      (-2*fr*nr)/(fr-nr),
+    0,      0,              -1,                    0) );  
+}
+
+
+/**
+ *  Create the matrix to transform the space into the camera view
+ *  orientation.  This translates and re-orients so that the camera
+ *  is at the origin, looking down the z-axis.
+ *   @param eye The vector locating camera in world frame
+ *   @param at  The vector locating the position in world frame at which the camera is looking
+ *   @param up  The vector indicating which way is "up" for the camera
+ **/
+function GetCameraViewOrientationMatrix(eye, at, up) {
+  // Get the normal vector, VPN p. 271, Angel & Schreiner
+  var n = subtract(eye,at);
+  n = normalize(n);
+
+  // Get the u vector in the view plane, p. 272, Angel & Schreiner
+  up = normalize(up);
+  var u = cross(up, n);
+  u = normalize(u);
+
+  // Get the v vector in the view plane, p. 272, Angel & Schreiner
+  var v = cross(n, u);
+  v = normalize(v);
+
+  // This is just to stay consistent with notation on pp. 269-271
+  var p = eye;
+
+  // Translate camera to origin
+  var T = mat4( 1, 0, 0, -p[0],
+                0, 1, 0, -p[1],
+                0, 0, 1, -p[2],
+                0, 0, 0, 1 );
+  
+  // This is the *reverse* of the coordinate trans we need
+  var A = mat4( u[0], v[0], n[0], 0,
+                u[1], v[1], n[1], 0,
+                u[2], v[2], n[2], 0,
+                0, 0, 0, 1 );             
+  A = mult(A,T);
+
+  // Invert to get the actual coord. rotation f
+  var M = inverse(A);
+
+  // Or just use the built-in function for this ...
+  //var M = lookAt(eye, at, up);
+  //M = inverse(M);
+  
+  // Return the view orientation change matrix for the camera
+  return (M);
+}
+
+
+
+/**
+ *  Draw our pyramid as a shell of triangle and square faces, using lines 
+ *  to do so.
+ */
+ function render(zCameraPosition) {
+
+  var modelMatrix = GetModelTransformationMatrix(30, -70, 90);
+  var cameraMatrix = GetCameraViewOrientationMatrix(vec3(0, 0, zCameraPosition),   // where is the camera?
+                                                    vec3(0, 0, 0),   // where is it looking?
+                                                    vec3(0, 1, 0) ); // Which way is up?
+  var perspMatrix = GetPerspectiveProjectionMatrix(45, -.1, .1);
+
+  // --- Now Draw the Polygon for the First Time ---
+  gl.uniformMatrix4fv( modelMatrixLoc, false, flatten(modelMatrix));
+  gl.uniformMatrix4fv( cameraMatrixLoc, false, flatten(cameraMatrix));
+  gl.uniformMatrix4fv( perspMatrixLoc, false, flatten(perspMatrix));  
+
+    // Clear the color & depth buffers
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Draw the faces
+    gl.drawArrays( gl.TRIANGLES, 0, pointLength );
+}
+
 
 
 /**
@@ -74,13 +176,14 @@ transMatrix = mult(transMatrix,ry);
     // Attach the GLSL code to the vertex shader, then compile it
     var vertexShaderCode =  "attribute vec4 vPosition;" +   // in-coming parameter
                             "attribute vec4 vColor;" +      // in-coming parameter
-                            "uniform mat4 uTransMatrix;" +  // Homogeneous transformation
+                            "uniform mat4 uModelMatrix;" +  // Homogeneous transformation
+                            "uniform mat4 uCameraMatrix;" + // Camera View transformation
                             "uniform mat4 uPerspMatrix;" +  // Perspective transformation
                             "varying vec4 fColor;" +        // Passing color variable
                             "void main() {" +
                             "    fColor = vColor;" +
                             //"    gl_Position = uTransMatrix * vPosition;" +
-                            "    gl_Position = uPerspMatrix * uTransMatrix * vPosition;" +
+                            "    gl_Position = uPerspMatrix * uCameraMatrix * uModelMatrix * vPosition;" +
                             "    gl_Position.x = gl_Position.x / gl_Position.w;" +
                             "    gl_Position.y = gl_Position.y / gl_Position.w;" +
                             "    gl_Position.z = gl_Position.z / gl_Position.w;" +
@@ -126,23 +229,11 @@ transMatrix = mult(transMatrix,ry);
     }
 
     // Grab the location for the matrices in the GPU
-    transMatrixLoc = gl.getUniformLocation( shaderProgram, "uTransMatrix" );
+    modelMatrixLoc = gl.getUniformLocation( shaderProgram, "uModelMatrix" );
+    cameraMatrixLoc = gl.getUniformLocation( shaderProgram, "uCameraMatrix" );
     perspMatrixLoc = gl.getUniformLocation( shaderProgram, "uPerspMatrix" );
 
     return shaderProgram;
-}
-
-
-/**
- *  Draw our pyramid as a shell of triangle and square faces, using lines 
- *  to do so.
- */
-function render() {
-    // Clear the color & depth buffers
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Draw the faces
-    gl.drawArrays( gl.TRIANGLES, 0, pointLength );
 }
 
 
@@ -180,23 +271,8 @@ function LoadDataOnGPU(gl, myData, shaderVariableStr, shaderVariableDim, shaderP
  *  based on the parameters set in the spinner box fields on the HTML page.
  */
 function setTranslationEventHandler() {
-    var tx = parseFloat(document.getElementById("translatex").value);
-    var ty = parseFloat(document.getElementById("translatey").value);
     var tz = parseFloat(document.getElementById("translatez").value);
-  
-    // Setup the translation transMatrix
-    var T = mat4( 1.0,  0.0,  0.0, tx,
-                  0.0,  1.0,  0.0, ty,
-                  0.0,  0.0,  1.0, tz,
-                  0.0,  0.0,  0.0, 1.0);  
-
-    // Concatenate with previous transformations:
-    transMatrix = mult(T, transMatrix);
-                   
-    // Send the transformation transMatrix to the GPU, then draw!
-    gl.uniformMatrix4fv( transMatrixLoc, false, flatten(transMatrix));
-    gl.uniformMatrix4fv( perspMatrixLoc, false, flatten(perspMatrix));
-    render();
+    render(tz);
   }
 
   
@@ -227,7 +303,7 @@ window.onload = function init()
 
 
     // --- Add Event Handlers ---
-    document.getElementById("translatebutton").onclick = setTranslationEventHandler
+    document.getElementById("translatez").oninput = setTranslationEventHandler
 
 
     // --- Create the Shape, then Load Point Data Onto GPU ---
@@ -259,7 +335,6 @@ window.onload = function init()
         vec4( 0.0,  0.0,  0.1,   1.0), // Tip
         vec4( 0.0, -0.1, -0.1,   1.0)
     ];
-    pointLength = polygonPoints.length;
 
     var pointColors = [
       [1.0, 0.0, 0.0, 0.8], // red  // Square face, T1
@@ -286,11 +361,8 @@ window.onload = function init()
     var vertexBufferId = LoadDataOnGPU(gl, polygonPoints, "vPosition", 4, shaderProgram);
     var colorBufferId = LoadDataOnGPU(gl, pointColors, "vColor", 4, shaderProgram  )
 
-
-    // --- Now Draw the Polygon for the First Time ---
-    gl.uniformMatrix4fv( transMatrixLoc, false, flatten(transMatrix));
-    gl.uniformMatrix4fv( perspMatrixLoc, false, flatten(perspMatrix));
-    render(gl, polygonPoints.length);
+    pointLength = polygonPoints.length;
+    render(0.5); //gl, polygonPoints.length);
 };
 
 
